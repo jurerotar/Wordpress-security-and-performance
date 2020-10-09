af0aa5968ddb3e9925faf91977d192e5454c140c
Jure Rotar <jrotar0@gmail.com>
2020-10-09 16:11:35 +0200

    Initial file upload

---
+++ wp-config.php
@@ -0,0 +1,11 @@
+<?php
+
+
+define('DISALLOW_FILE_EDIT',true);
+define('FORCE_SSL_ADMIN', true);
+define('WP_DEBUG', false);
+define('WP_DEBUG_DISPLAY', false);
+define('WP_AUTO_UPDATE_CORE', true);
+
+
+?>
